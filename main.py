93cc0959462765ee727f3292e1f47353b4407c49
viswessvaran06-afk <viswessvaran06@gmail.com>
2026-06-10 12:29:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from app.routes.gateway import router
 
-app = FastAPI(title="Enterprise AI Security Gateway")
-
-app.include_router(router, prefix="/api/v1")
+app = FastAPI(
+    title="Enterprise AI Security Gateway"
+)
 
 @app.get("/")
 def home():
